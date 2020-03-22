9a9bc8836dcf2f4012ae1800fb2a4f4c9deaba15
Eugene <eu.rychkov@yahoo.com>
2020-03-22 17:18:33 +0300

    Fix absolute directory

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
     //File to locate to the login page
-    header('Location: http://esp32.tfeya.ru/login.php',true, 301);
+    header('Location: ./login.php',true, 301);
     //to avoid cookies, you should use no_login.php page instead. 
     //Example of the GET-request: http://esp32.tfeya.ru/no_login.php?login=kra&val=50
 ?>
